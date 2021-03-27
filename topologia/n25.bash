9100533c8f398471580c5adf31c5bb3f9e23b8c9
Fernando Villanueva <sn2005ap@gmail.com>
2021-03-27 11:49:18 -0300

    fixes

--- topologia/n25.bash
+++ topologia/n25.bash
@@ -1,4 +1,4 @@
-ifconfig eth1 10.0.17.2/24
 ifconfig eth2 10.0.28.1/24
 ifconfig eth0 10.0.52.1/24
+ifconfig eth1 10.0.17.2/24
 
--- topologia/n4.bash
+++ topologia/n4.bash
@@ -1,4 +1,4 @@
-ifconfig eth0 10.0.0.2/24
 ifconfig eth3 10.0.27.1/24
 ifconfig eth1 10.0.50.1/24
+ifconfig eth0 10.0.0.2/24
 
